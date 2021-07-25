c2a2eddc8c55a0f08e9a55596dba2bce01e2d40c
Gerardo Perrucci <centrodph@gmail.com>
2021-07-25 16:27:37 +0000

    covid death graph v1

--- covid_us_daily.py
+++ covid_us_daily.py
@@ -1,6 +1,17 @@
 import requests
+import matplotlib.pyplot as plt
 
 
-r = requests.get('url')
-
-print(r.json())
+r = requests.get('https://api.covidtracking.com/v1/us/daily.json')
+d = r.json()
+dates = []
+death = []
+for i in range(len(d)):
+  if d[i]['death'] != None:
+    dates.append(i)
+    death.append(d[i]['death'])
+    print(d[i]['death'])
+death.reverse()
+plt.yscale('log') 
+plt.plot(dates, death)
+plt.show()
